dfd56eeb372114a959138d8e31e7669b927dd4e0
Francesco <francesco.bez@gmail.com>
2021-05-14 15:45:25 +0200

    added sntp example with rome time zone

--- examples/rtc_sntp/main/sntp_example_main.c
+++ examples/rtc_sntp/main/sntp_example_main.c
@@ -20,6 +20,10 @@
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
 #include "esp_sntp.h"
+#include "conversions.h"
+
+#include "rx8010sj.h"
+
 
 static const char *TAG = "example";
 
@@ -48,9 +52,10 @@ void time_sync_notification_cb(struct timeval *tv)
 
 void app_main(void)
 {
+    esp_err_t ret;
     ++boot_count;
     ESP_LOGI(TAG, "Boot count: %d", boot_count);
-
+    ret = rx8010_init();
     time_t now;
     struct tm timeinfo;
     time(&now);
@@ -61,55 +66,23 @@ void app_main(void)
         obtain_time();
         // update 'now' variable with current time
         time(&now);
-    }
-#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
-    else {
-        // add 500 ms error to the current system time.
-        // Only to demonstrate a work of adjusting method!
-        {
-            ESP_LOGI(TAG, "Add a error for test adjtime");
-            struct timeval tv_now;
-            gettimeofday(&tv_now, NULL);
-            int64_t cpu_time = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
-            int64_t error_time = cpu_time + 500 * 1000L;
-            struct timeval tv_error = { .tv_sec = error_time / 1000000L, .tv_usec = error_time % 1000000L };
-            settimeofday(&tv_error, NULL);
-        }
+        localtime_r(&now, &timeinfo);
+        rx8010_set_time(&timeinfo);
 
-        ESP_LOGI(TAG, "Time was set, now just adjusting it. Use SMOOTH SYNC method.");
-        obtain_time();
-        // update 'now' variable with current time
-        time(&now);
     }
-#endif
 
+    struct tm rtc_time ;
+
+    rx8010_get_time(&rtc_time);
     char strftime_buf[64];
 
     // Set timezone to Eastern Standard Time and print local time
-    setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
+    setenv("TZ", "GMT-2", 1);
     tzset();
-    localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
+    localtime_r(&now, &rtc_time);
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &rtc_time);
+    ESP_LOGI(TAG, "The current RTC date/time  in Amsterdam/Rome is: %s", strftime_buf);
 
-    // Set timezone to China Standard Time
-    setenv("TZ", "CST-8", 1);
-    tzset();
-    localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    ESP_LOGI(TAG, "The current date/time in Shanghai is: %s", strftime_buf);
-
-    if (sntp_get_sync_mode() == SNTP_SYNC_MODE_SMOOTH) {
-        struct timeval outdelta;
-        while (sntp_get_sync_status() == SNTP_SYNC_STATUS_IN_PROGRESS) {
-            adjtime(NULL, &outdelta);
-            ESP_LOGI(TAG, "Waiting for adjusting time ... outdelta = %li sec: %li ms: %li us",
-                        (long)outdelta.tv_sec,
-                        outdelta.tv_usec/1000,
-                        outdelta.tv_usec%1000);
-            vTaskDelay(2000 / portTICK_PERIOD_MS);
-        }
-    }
 
     const int deep_sleep_sec = 10;
     ESP_LOGI(TAG, "Entering deep sleep for %d seconds", deep_sleep_sec);
